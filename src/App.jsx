7fd8006b3fffc021264ac6e5d502aaf7074cfe99
dlesk1 <dleskdomagoj@gmail.com>
2024-09-16 10:54:11 +0200

    SolidJs_Skola

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+import MyComponent from './MyComponent';
+
+export default function App() {
+  return (
+    <>
+    <div class={styles.App}> Dobar dan, svijete! </div>
+    <MyComponent />
+    </>
+  );
+}
+
+
